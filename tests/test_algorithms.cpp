5ead4ed6b16dea0be8e81d35c553d98f202825e0
Rohan Harpalani <rohanhh2@illinois.edu>
2021-12-06 10:49:39 -0600

    dijkstra tests

--- tests/test_algorithms.cpp
+++ tests/test_algorithms.cpp
@@ -1,3 +1,5 @@
+// TODO: IDDFS Tests
+
 #include <string>
 #include <vector>
 #include <iostream>
@@ -50,36 +52,6 @@ TEST_CASE("DFS") {
     REQUIRE(expected == outcome);
 }
 
-TEST_CASE("Cycle DFS") {
-    /* Graph:
-    *         A    
-    *     /   |   \   
-    *    B    C    E  
-    *          \  / \ 
-    *            D   F
-    */
-   
-    Graph graph;
-    graph.InsertVertex("A");
-    graph.InsertVertex("B");
-    graph.InsertVertex("C");
-    graph.InsertVertex("D");
-    graph.InsertVertex("E");
-    graph.InsertVertex("F");
-    graph.InsertEdge("A","E", 1);
-    graph.InsertEdge("A","C", 1);
-    graph.InsertEdge("A","B", 1);
-    graph.InsertEdge("C","D", 1);
-    graph.InsertEdge("D","E", 1);
-    graph.InsertEdge("E","F", 1);
-
-    algorithms dfs(graph, "A");
-
-    vector<string> outcome = dfs.traverse();
-    vector<string> expected = {"A", "B", "C", "D", "E", "F"};
-    REQUIRE(outcome == expected);
-}
-
 TEST_CASE("Complex DFS") {
     /* Graph:
     *        A        
@@ -112,6 +84,36 @@ TEST_CASE("Complex DFS") {
 
     vector<string> outcome = dfs.traverse();
     vector<string> expected = {"A", "B", "C", "D", "E", "F", "G"};
+    REQUIRE(outcome == expected);
+}
+
+TEST_CASE("Cycle DFS") {
+    /* Graph:
+    *         A    
+    *     /   |   \   
+    *    B    C    E  
+    *          \  / \ 
+    *            D   F
+    */
+   
+    Graph graph;
+    graph.InsertVertex("A");
+    graph.InsertVertex("B");
+    graph.InsertVertex("C");
+    graph.InsertVertex("D");
+    graph.InsertVertex("E");
+    graph.InsertVertex("F");
+    graph.InsertEdge("A","E", 1);
+    graph.InsertEdge("A","C", 1);
+    graph.InsertEdge("A","B", 1);
+    graph.InsertEdge("C","D", 1);
+    graph.InsertEdge("D","E", 1);
+    graph.InsertEdge("E","F", 1);
+
+    algorithms dfs(graph, "A");
+
+    vector<string> outcome = dfs.traverse();
+    vector<string> expected = {"A", "B", "C", "D", "E", "F"};
     REQUIRE(outcome == expected);
 }
 
@@ -154,8 +156,7 @@ TEST_CASE("Dijkstras") {
     graph.InsertEdge("L","M",1);
     
     algorithms dijkstra(graph, "1");
-    vector<string> outcome;
-    vector<string> expected;
+    vector<string> outcome, expected;
 
     outcome = dijkstra.Dijkstras("A", "B");
     expected = {"A", "B"};
@@ -175,5 +176,96 @@ TEST_CASE("Dijkstras") {
 
     outcome = dijkstra.Dijkstras("H", "I");
     expected = {"H", "D", "B", "A", "I"};
+    REQUIRE(outcome == expected);
+}
+
+TEST_CASE("Complex Dijkstras") {
+    /* Graph:
+    *        A        
+    *       / \       
+    *      B   F           
+    *      | / |      
+    *      C---E--G      
+    *       \ /       
+    *        D        
+    */
+
+    Graph graph;
+    graph.InsertVertex("A");
+    graph.InsertVertex("B");
+    graph.InsertVertex("C");
+    graph.InsertVertex("D");
+    graph.InsertVertex("E");
+    graph.InsertVertex("F");
+    graph.InsertVertex("G");
+    graph.InsertEdge("A","B", 1);
+    graph.InsertEdge("B","C", 1);
+    graph.InsertEdge("C","F", 1);
+    graph.InsertEdge("C","E", 1);
+    graph.InsertEdge("C","D", 1);
+    graph.InsertEdge("E","G", 1);
+    graph.InsertEdge("E","F", 1);
+    graph.InsertEdge("F","A", 1);
+
+    algorithms dijkstra(graph, "1");
+    vector<string> outcome, expected;
+
+    outcome = dijkstra.Dijkstras("A", "C");
+    expected = {"A", "B", "C"};
+    REQUIRE(outcome == expected);
+
+    outcome = dijkstra.Dijkstras("B", "G");
+    expected = {"B", "C", "E", "G"};
+    REQUIRE(outcome == expected);
+
+    outcome = dijkstra.Dijkstras("F", "G");
+    expected = {"F", "E", "G"};
+    REQUIRE(outcome == expected);
+    
+    outcome = dijkstra.Dijkstras("B", "F");
+    expected = {"B", "A", "F"};
+    REQUIRE(outcome == expected);
+}
+
+TEST_CASE("Cycle Dijkstras") {
+    /* Graph:
+    *         A    
+    *     /   |   \   
+    *    B    C    E  
+    *          \  / \ 
+    *            D   F
+    */
+   
+    Graph graph;
+    graph.InsertVertex("A");
+    graph.InsertVertex("B");
+    graph.InsertVertex("C");
+    graph.InsertVertex("D");
+    graph.InsertVertex("E");
+    graph.InsertVertex("F");
+    graph.InsertEdge("A","E", 1);
+    graph.InsertEdge("A","C", 1);
+    graph.InsertEdge("A","B", 1);
+    graph.InsertEdge("C","D", 1);
+    graph.InsertEdge("D","E", 1);
+    graph.InsertEdge("E","F", 1);
+
+    algorithms dijkstra(graph, "A");
+    vector<string> outcome, expected;
+
+    outcome = dijkstra.Dijkstras("A", "B");
+    expected = {"A", "B"};
+    REQUIRE(outcome == expected);
+
+    outcome = dijkstra.Dijkstras("A", "D");
+    expected = {"A", "C", "D"};
+    REQUIRE(outcome == expected);
+
+    outcome = dijkstra.Dijkstras("A", "E");
+    expected = {"A", "E"};
+    REQUIRE(outcome == expected);
+
+    outcome = dijkstra.Dijkstras("A", "F");
+    expected = {"A", "E", "F"};
     REQUIRE(outcome == expected);
 }
--- tests/test_parsing.cpp
+++ tests/test_parsing.cpp
@@ -1,4 +1,4 @@
-// TODO
+// TODO: Edge Tests
 
 #include <string>
 #include <vector>
