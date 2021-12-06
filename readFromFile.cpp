425fe563487eed6797939a337ad90fdacb5e9afb
Sathvik Perkari <sperka2@siebl-0224-08.ews.illinois.edu>
2021-12-05 20:39:28 -0600

    readFromFile

--- readFromFile.cpp
+++ readFromFile.cpp
@@ -16,3 +16,17 @@ std::string file_to_string(const std::string & filename) {
 
 	return strStream.str();
 }
+std::vector<std::string> file_to_vector(const std::string & filename) {
+	std::ifstream text(filename);
+	std::vector<std::string> out;
+
+	if (text.is_open()) {
+		std::istream_iterator<std::string> iter(text);
+		while (!text.eof()) {
+			out.push_back(*iter);
+			++iter;
+		}
+	}
+
+	return out;
+} 
